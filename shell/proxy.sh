b7e6313687ba52100c339bc2a296fc61ad42c8d6
Peter Löcker <peter.loecker@live.at>
2016-04-18 17:33:50 +0200

    proxy

---
+++ shell/proxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#networksetup -getwebproxy Ethernet
+networksetup -setwebproxy Ethernet 172.16.1.64 3128
+networksetup -setsecurewebproxy Ethernet 172.16.1.64 3128
+
+#networksetup -getwebproxy Ethernet
+#networksetup -getsecurewebproxy Ethernet
+
+read -p "Press any key to close this terminal"
+
+networksetup -setwebproxy Ethernet off
+networksetup -setsecurewebproxy Ethernet off
