9d95a967b55ae31c6c2695310acd72af1d320cc5
PeterBrain <peter.loecker@live.at>
2016-05-17 00:11:35 +0200

    keyboard

--- shell/keyboard_switch.sh
+++ shell/keyboard_switch.sh
@@ -6,4 +6,6 @@ echo "you may have to use an external keyboard for this"
 echo "To proceed, press enter"
 read
 
-sudo kextunload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/ || sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/
+sudo kextunload -b com.apple.driver.AppleUSBTCKeyboard || kextload -b com.apple.driver.AppleUSBTCKeyboard
+
+#sudo kextunload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/ || sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/
