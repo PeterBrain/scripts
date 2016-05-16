8274245a2b0cc37c546a4573ae08db34ce1734f0
PeterBrain <peter.loecker@live.at>
2016-05-16 23:14:03 +0200

    turn keyboard on/off

---
+++ shell/keyboard_switch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "To turn your internal keyboard back on, simply run the script again"
+echo "you may have to use an external keyboard for this"
+
+echo "To proceed, press enter"
+read
+
+sudo kextunload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/ || sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/
