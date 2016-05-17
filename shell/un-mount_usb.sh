2bf31a13f7f3097caa24d1a6c77869d0a6d77ca0
PeterBrain <peter.loecker@live.at>
2016-05-17 16:51:32 +0200

    mount unmount usb

---
+++ shell/un-mount_usb.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo
+echo "unmount USB drives"
+echo "Type 0 to unmount a drive"
+
+read var
+
+diskutil list
+
+echo
+echo "Type in the name of the drive you want to unmount"
+read usb
+echo
+
+if [ "$var" == "0" ]; then
+
+diskutil unmount "$usb"
+#diskutil unmount /dev/disk4s1
+
+else
+
+diskutil mount "$usb"
+
+fi
+
+#df
+#if [ "/Volumes/$usb" ]; then
