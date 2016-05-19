6ffb69e0ea1f213681a803bb585819b1e6018d5e
PeterBrain <peter.loecker@live.at>
2016-05-19 18:56:28 +0200

    dummy file
    file size, name, extension and path is now editable

--- shell/dummy_file.sh
+++ shell/dummy_file.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
+echo
 echo "Make a dummy file with one or several bytes (b), kilobytes (k), megabytes (m), gigabytes (g)"
+echo
 
-read
+echo "Type in a filesize (1 Gigabyte = 1g)"
+read filesize
 
+echo "Name the File:"
+read name
+
+echo "Give it an extension"
+read extension
+
+echo "Choose a storage directory (leave blank for desktop)"
+read path
+
+if [ -n $path ]; then
 cd Desktop
-mkfile 1g test.abc
+else
+cd $path
+fi
+
+mkfile $filesize $name.$extension
+#mkfile 1g test.abc
