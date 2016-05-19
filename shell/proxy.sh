e44f9e5207d06842d40a51fde446669b8dfaddd0
PeterBrain <peter.loecker@live.at>
2016-05-19 19:01:17 +0200

    proxy
    better user experience

--- shell/proxy.sh
+++ shell/proxy.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+echo
+echo "Change proxy setting for work"
+echo
+
 #networksetup -getwebproxy Ethernet
 networksetup -setwebproxy Ethernet 172.16.1.64 3128
 networksetup -setsecurewebproxy Ethernet 172.16.1.64 3128
 
+# Displays the current proxy settings
 #networksetup -getwebproxy Ethernet
 #networksetup -getsecurewebproxy Ethernet
 
-read -p "Press any key to close this terminal"
+read -p "Press enter to close this terminal and revert proxy settings"
 
 networksetup -setwebproxy Ethernet off
 networksetup -setsecurewebproxy Ethernet off
