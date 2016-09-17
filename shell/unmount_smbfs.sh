56db0e12cacb42be0b875a8c4473e8202da199b4
PeterBrain <peter.loecker@live.at>
2016-09-17 13:45:19 +0200

    unmount_smbfs
    simplified

--- shell/unmount_smbfs.sh
+++ shell/unmount_smbfs.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-drives="Public www$ Peter$ bin"
+:`
+drives="Public www$ Peter$ Bilder Games Movies"
 
-#for drive in $drives
-#do
+for drive in $drives
+do
+
+drive_mk=(${drive//$/})
+umount -t smbfs /tmp/share/${drive_mk[0]}
+rmdir /tmp/share/${drive_mk[0]}
+
+done
+`
 
 umount -A -t smbfs
 
-#drive_mk=(${drive//$/})
-#umount -t smbfs /tmp/share/${drive_mk[0]}
-#rmdir /tmp/share/${drive_mk[0]}
+rm -rf /tmp/share
 
-#done
+#rmdir /tmp/share
 
-rmdir /tmp/share
-
-read -p "Press enter to close this terminal"
+#read -p "Press enter to close this terminal"
