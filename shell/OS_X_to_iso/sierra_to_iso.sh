cfee2ffaa7566fcf9ded982f3ed65957fc5de7b2
PeterBrain <peter.loecker@live.at>
2016-09-21 21:46:39 +0200

    sierra_to_iso
    this is the command to create a bootable device with macOS Sierra on it

---
+++ shell/OS_X_to_iso/sierra_to_iso.sh
@@ -0,0 +1,1 @@
+sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/Untitled --applicationpath /Applications/Install\ macOS\ Sierra.app --nointeraction
