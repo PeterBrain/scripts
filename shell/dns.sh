dd119a546cc44bc7056a6aeaad310ee7ce7ff03f
PeterBrain <peter.loecker@live.at>
2018-10-17 21:32:35 +0200

    vscode + dns settings
    start vscode without gpu acceleration
    toggle dns settings between none and cloudflare 1.1.1.1

---
+++ shell/dns.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+dns_status=`networksetup -getdnsservers Wi-Fi`
+
+if [ "$dns_status" != "There aren't any DNS Servers set on Wi-Fi." ]
+then
+  networksetup -getdnsservers > ~/.dns_servers
+  networksetup -setdnsservers Wi-Fi empty
+else
+  networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001
+fi
