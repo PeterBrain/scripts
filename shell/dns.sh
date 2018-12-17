8f6274f5b014e9bca62e2b6ef19f2ff513f63d47
PeterBrain <peter.loecker@live.at>
2018-12-17 22:38:17 +0100

    dns & routing table

--- shell/dns.sh
+++ shell/dns.sh
@@ -4,7 +4,7 @@ dns_status=`networksetup -getdnsservers Wi-Fi`
 
 if [ "$dns_status" != "There aren't any DNS Servers set on Wi-Fi." ]
 then
-  networksetup -getdnsservers > ~/.dns_servers
+  networksetup -getdnsservers Wi-Fi > ~/.dns_servers
   networksetup -setdnsservers Wi-Fi empty
 else
   networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001
