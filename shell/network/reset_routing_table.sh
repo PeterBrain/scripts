6be9eadbb4584fed4896561a63fda938c05bfe8f
PeterBrain <peter.loecker@live.at>
2019-05-19 12:27:20 +0200

    added notification & optimized workflow

--- shell/network/reset_routing_table.sh
+++ shell/network/reset_routing_table.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ## reset routing table on macOS
 
+sudo killall -HUP mDNSResponder # flush dns
+
 #netstat -r ## display routing table
 
 for i in {0..5}; do
@@ -12,3 +14,5 @@ sudo ifconfig en0 down # bring interface down
 sleep 1
 
 sudo ifconfig en0 up # bring interface up again
+
+osascript -e 'display notification "DNS flushed & routing table reset" with title "System"'
--- shell/network/term_proc_net_share.sh
+++ shell/network/term_proc_net_share.sh
@@ -9,3 +9,5 @@ read -p "Type in the drive you want to disconnect: " share
 x=`lsof -Fp /Volumes/$share | grep 'p'`
 kill -9 "${x##p}"
 
+diskutil umount /Volumes/$share
+
