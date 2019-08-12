9bf23dde173c845f4d8efa2dae40c52552dc734b
PeterBrain <peter.loecker@live.at>
2019-08-12 20:56:47 +0200

    bcc bug fixed

--- shell/disk_space_alert.sh
+++ shell/disk_space_alert.sh
@@ -5,7 +5,7 @@ CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
 THRESHOLD=90
 
 if [ "$CURRENT" -gt "$THRESHOLD" ]; then
-  mail -s 'Disk Space Alert' to@example.com -b bcc@example.com << EOF
+  mail -s 'Disk Space Alert' -b bcc@example.com to@example.com << EOF
 Your root partition remaining free space is critically low. Used: $CURRENT%
 
 $STATUS
