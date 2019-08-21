d5bec1902aad5f102a4c7c321a07425388efd9f7
PeterBrain <peter.loecker@live.at>
2019-08-21 21:17:39 +0200

    echo fix

--- shell/backup_www.sh
+++ shell/backup_www.sh
@@ -15,19 +15,19 @@ ROOT_DIR="/var/www/${DOMAIN}"
 DATE=$(date '+%Y-%m-%d_%H-%M-%S')
 
 if ([ -z "${MODE}" ] || [ "${MODE}" == "full" ] || [ "${MODE}" == "db" ]) && [ -n "${DATABASE}" ]; then
-  echo "\nBacking up Database\n"
+  echo -e "\nBacking up Database\n"
   mysqldump --defaults-group-suffix=${DOMAIN} ${DATABASE} | gzip > ${ROOT_DIR}/backup/${DOMAIN}_${DATE}_${DATABASE}.sql.gz
 
   # clean db_logs - implement in magento_bin.sh
 fi
 
 if [ -z "${MODE}" ] || [ "${MODE}" == "full" ] || [ "${MODE}" == "dir" ]; then
-  echo "\nClearing cache\n"
+  echo -e "\nClearing cache\n"
   ./magento_bin.sh ${DOMAIN} cache > /dev/null
 
-  echo "\nBacking up Files\n"
+  echo -e "\nBacking up Files\n"
   tar $(if [ ! -z "${EXCLUDE_DIR}" ]; then echo --exclude=${ROOT_DIR}/web/${EXCLUDE_DIR}; fi) -zcf ${ROOT_DIR}/backup/${DOMAIN}_${DATE}.tar.gz ${ROOT_DIR}/web
 
-  echo "\nCleaning log files\n"
+  echo -e "\nCleaning log files\n"
   ./magento_bin.sh ${DOMAIN} log
 fi
