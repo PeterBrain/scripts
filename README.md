3fa27ced35214414976ff3242c485813e7afaf32
PeterBrain <peter.loecker@live.at>
2020-02-05 19:44:27 +0100

    touchid update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Scripts
 This is a collection of all my Shell, Batch, PowerShell and Visual Basic scripts.
 
-Everything that needs a long time and has to be done very often is now a script.
+Everything that needs a long time doing manually and has to be done very often is now a script.
 At least in my case.
 Most of them are intended to run on macOS and Windows.
--- shell/macOS/touchID_shell.sh
+++ shell/macOS/touchID_shell.sh
@@ -8,4 +8,6 @@ path="/etc/pam.d/sudo" # path to file for auth expression
 if [ -z `grep -lr "${auth_expr}" $path` ]; then # false, if the auth expression is already there
   sudo cp $path "${path}.bkp" # create backup file of the old configuration
   sudo sed -i '' -e "1s/^//p; 1s/^.*/${auth_expr}/" $path # duplicated first row and replaces it with the auth expression
+else
+  echo "The script ran once already - nothing to do here"
 fi
