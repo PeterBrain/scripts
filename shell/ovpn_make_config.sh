0fcecafbcad56f7b5803b72d8240eef305202cdf
PeterBrain <peter.loecker@live.at>
2020-02-10 14:53:02 +0100

    openvpn client profile generator
    with keys and certificates

---
+++ shell/ovpn_make_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# First argument: Client identifier
+
+KEY_DIR=keys
+OUTPUT_DIR=files
+BASE_CONFIG=template.ovpn
+
+if [ ! -d "${OUTPUT_DIR}" ]; then
+  mkdir ${OUTPUT_DIR}
+fi
+
+cat ${BASE_CONFIG} \
+  <(echo -e '<ca>') \
+  ${KEY_DIR}/ca.crt \
+  <(echo -e '</ca>\n<cert>') \
+  ${KEY_DIR}/${1}.crt \
+  <(echo -e '</cert>\n<key>') \
+  ${KEY_DIR}/${1}.key \
+  <(echo -e '</key>\n<tls-auth>') \
+  ${KEY_DIR}/ta.key \
+  <(echo -e '</tls-auth>') \
+  > ${OUTPUT_DIR}/${1}.ovpn
