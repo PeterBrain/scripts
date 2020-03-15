2ae338402bdc851132ef1ada5d03be9400610a3b
PeterBrain <peter.loecker@live.at>
2020-03-15 11:42:36 +0100

    esp tripod lamp

---
+++ shell/tripod_on.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+response=0
+retries=0
+
+while [ "${response}" != 200 -a "${retries}" -lt 3 ]
+do
+  response=$(curl --head --write-out %{http_code} --silent --output /dev/null --max-time 3 "http://192.168.24.195/rf3/on")
+  let retries=$retries+1
+done
