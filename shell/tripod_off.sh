d011c2a48926e8e8e3789df6a22cce9261dcb2f9
PeterBrain <peter.loecker@live.at>
2020-03-15 13:11:16 +0100

    tripod

--- shell/tripod_off.sh
+++ shell/tripod_off.sh
@@ -2,9 +2,10 @@
 
 response=0
 retries=0
+uri="http://192.168.24.195/rf3/off"
 
 while [ "${response}" != 200 -a "${retries}" -lt 3 ]
 do
-  response=$(curl --head --write-out %{http_code} --silent --output /dev/null --max-time 3 "http://192.168.24.195/rf3/off")
+  response=$(curl --head --write-out %{http_code} --silent --output /dev/null --max-time 3 ${uri})
   let retries=$retries+1
 done
