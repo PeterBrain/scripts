eb679d7b17d85c0571fe564036b879bbdb733465
PeterBrain <peter.loecker@live.at>
2020-04-05 22:31:12 +0200

    script update

--- shell/macOS/open_path.sh
+++ shell/macOS/open_path.sh
@@ -5,14 +5,18 @@ printf "\nOpen a predefined path\n\n"
 PS3="Which one do you want to open? (or ctrl-c to quit): "
 
 directories=( \
-  "/desktop/" \
-  "/private/etc/" \
-  "/tmp/" \
-  "/Users/$USER/.taco_home/" \
-  "/Users/$USER/.npm" \
-  "/Users/$USER/.homebridge" \
-  "/Users/$USER/Library/Mobile Documents/com~apple~CloudDocs" \
-  "/Library/WebServer/Documents" \
+  "/var/" # variable data (log files)
+  "/tmp/" # temporary files, cache
+  "/etc/" # host-specific configuration (hosts)
+  "/dev/" # device files
+  "/usr/" # unix system resources, cli binaries, libraries
+  "/Volumes/" # mounted devices
+  "/Library/" # system library
+  "/Applications/" # system applications
+  "/Users/$USER/Library/" # user library
+  #"/Users/$USER/Applications/" # user applications
+  "/Users/$USER/Library/Mobile Documents/com~apple~CloudDocs" # iCloud
+  "/Library/WebServer/Documents" # webserver
 )
 
 select directory in "${directories[@]}"
